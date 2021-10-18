d1e9a629c13c43de66b798d83540c6de8036796e
Ryan Hawkins <rkhawkins80@hotmail.com>
2021-10-18 16:23:10 +0000

    Updated CSS and changed some variables to add a second messaging div.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const $$ = (q) => [...document.querySelectorAll(q)]
 // Set variables.
 const header = $("header")
 const briefcases = $$(".briefcases")
-const message = $("#message")
+const instructions = $("#instructions")
 
 
 // Set event listeners.
@@ -27,21 +27,35 @@ window.onload = () => {
     playGame();
 }
 
-
+let briefcasesSelected = []
+let firstCase = ""
+const ORIGINALMESSAGE = "Let's plays Deal or No Deal"
+const ORIGINALINSTRUCTIONS = "Choose a briefcase to start with."
 
 // Start the logic.
 
 function playGame() {
+    firstCase = ""
     briefcases.forEach((e) => e.style.display = "initial")
     let round = 1
-    let briefcasesSelected = []
+    briefcasesSelected = []
     console.log("Started game.")
     let offerAccepted = false;
+    instructions.innerText = ORIGINALINSTRUCTIONS
+
+    
 
 }
 
 function chooseBriefcase(briefcase, round = 1) {
-    briefcase.style.display = "none"
+
+    if (briefcasesSelected.length == 0) {
+        firstCase = briefcase;
+        instructions.innerText = `You chose briefcase #${firstCase.id}.`
+    }
+    briefcase.style.display = "none";
+    briefcasesSelected.push(briefcase.id);
+    console.log(`Cases selected:  ${briefcasesSelected}`)
 }
 
 function getBankOffer(round, highestCaseAmount) {
